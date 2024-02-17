4fbaf4ebd6c4fc06db34eb0d4d698f711c392f7a
tracer-ics2023 <tracer@njuics.org>
2024-02-17 17:39:34 +0800

    >  compile NEMU 20240207001 kerhac Linux roger-virtual-machine 6.5.0-17-generic #17~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jan 16 14:32:32 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:39:34 up  5:30,  8 users,  load average: 0.22, 0.21, 0.19

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -42,7 +42,7 @@ run: run-env
 	@echo "Roger test point 001 end"
 	@echo $(NEMU_EXEC)
 	@echo "Roger test point 002 start"
-	@sh $(NEMU_EXEC)
+	$(NEMU_EXEC)
 	@echo "Roger test point 002 end"
 
 gdb: run-env
